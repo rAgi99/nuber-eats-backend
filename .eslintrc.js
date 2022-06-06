756a9bc0e5fe67c3b9998089fea9da5d575e1c4f
rAgi99 <gunmo2929@naver.com>
2022-06-06 21:28:44 +0900

    0606 setup done

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,19 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+  extends: ['prettier'],
   root: true,
   env: {
     node: true,
