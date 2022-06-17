a524f6ae635946139580f14c168e9f4eefe8a08f
rAgi99 <gunmo2929@naver.com>
2022-06-17 15:04:17 +0900

    testing createAccount

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -4,6 +4,14 @@ import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 import { getConnection } from 'typeorm';
 
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
+const GRAPHQL_ENDPOINT = '/graphql';
+
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
 
@@ -21,10 +29,68 @@ describe('UserModule (e2e)', () => {
     app.close();
   });
 
-  it.todo('createAccount');
-  it.todo('login');
-  it.todo('me');
+  describe('createAccount', () => {
+    const EMAIL = 'nico@las.com';
+    it('should create account', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(
+              input: { 
+                email: "${EMAIL}", 
+                password: "12345", 
+                role: Owner 
+              }) {
+              ok
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(true);
+          expect(res.body.data.createAccount.error).toBe(null);
+        });
+    });
+
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+          mutation {
+            createAccount(
+              input: { 
+                email: "${EMAIL}", 
+                password: "12345", 
+                role: Owner 
+              }) {
+              ok
+              error
+            }
+          }
+          `,
+        })
+        .expect(200)
+        .expect((res) => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toBe(
+            'There is a user with that email already',
+          );
+        });
+    });
+  });
+
   it.todo('userProfile');
+
+  it.todo('login');
+
+  it.todo('me');
+
   it.todo('verifyEmail');
+
   it.todo('editProfile');
 });
