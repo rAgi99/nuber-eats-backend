6857d995ba492225d46a532848a74deb4825ce1f
rAgi99 <gunmo2929@naver.com>
2022-06-20 19:42:06 +0900

    Create Order Part one

--- src/orders/orders.resolver.ts
+++ src/orders/orders.resolver.ts
@@ -1,6 +1,7 @@
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { User } from 'aws-sdk/clients/budgets';
 import { AuthUser } from 'src/auth/auth-user.decorator';
+import { Role } from 'src/auth/role.decorator';
 import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
 import { Order } from './entities/order.entity';
 import { OrderService } from './orders.service';
@@ -10,6 +11,7 @@ export class OrderResolver {
   constructor(private readonly ordersService: OrderService) {}
 
   @Mutation((returns) => CreateOrderOutput)
+  @Role(['Client'])
   async createOrder(
     @AuthUser() customer: User,
     @Args('input')
