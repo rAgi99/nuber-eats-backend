aed70abbfa5f367aa1ab9b24a823e410f916219d
rAgi99 <gunmo2929@naver.com>
2022-06-20 19:41:34 +0900

    Create Order Part one

---
+++ src/orders/orders.resolver.ts
@@ -0,0 +1,22 @@
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { User } from 'aws-sdk/clients/budgets';
+import { AuthUser } from 'src/auth/auth-user.decorator';
+import { CreateOrderInput, CreateOrderOutput } from './dtos/create-order.dto';
+import { Order } from './entities/order.entity';
+import { OrderService } from './orders.service';
+
+@Resolver((of) => Order)
+export class OrderResolver {
+  constructor(private readonly ordersService: OrderService) {}
+
+  @Mutation((returns) => CreateOrderOutput)
+  async createOrder(
+    @AuthUser() customer: User,
+    @Args('input')
+    createOrderInput: CreateOrderInput,
+  ): Promise<CreateOrderOutput> {
+    return {
+      ok: true,
+    };
+  }
+}
