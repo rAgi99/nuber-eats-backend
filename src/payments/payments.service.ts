cdf209c09f2e86b610a80c907e5f507a9c38b4a6
rAgi99 <gunmo2929@naver.com>
2022-06-21 17:03:16 +0900

    Promoting Restaurants part two

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Restaurant } from 'src/restaurants/entities/restaurant.entity';
 import { User } from 'src/users/entities/user.entity';
-import { Repository } from 'typeorm';
+import { LessThan, Repository } from 'typeorm';
 import {
   CreatePaymentInput,
   CreatePaymentOutput,
@@ -70,5 +71,19 @@ export class PaymentService {
         error: 'Could not load payments',
       };
     }
+  }
+
+  @Cron('0 6 * * *')
+  async checkPromotedRestaurants() {
+    const restaurants = await this.restaurants.find({
+      isPromoted: true,
+      promotedUntil: LessThan(new Date()),
+    });
+    console.log(restaurants);
+    restaurants.forEach(async (restaurant) => {
+      restaurant.isPromoted = false;
+      restaurant.promotedUntil = null;
+      await this.restaurants.save(restaurant);
+    });
   }
 }
