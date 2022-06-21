a8442b2daecb0548dd0c566599c5b581d6bf9c22
rAgi99 <gunmo2929@naver.com>
2022-06-21 15:53:15 +0900

    createPayment part one

---
+++ src/payments/payments.resolver.ts
@@ -0,0 +1,12 @@
+import { Mutation, Resolver } from '@nestjs/graphql';
+import { Payment } from './entities/payment.entity';
+import { PaymentService } from './payments.service';
+
+@Resolver((of) => Payment)
+export class PaymentResolver {
+  constructor(private readonly paymentService: PaymentService) {
+
+    @Mutation(returns => )
+
+  }
+}
