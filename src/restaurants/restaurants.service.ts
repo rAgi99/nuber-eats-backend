cd221484999d02528157a89c88f9a3bb8e8c02b6
rAgi99 <gunmo2929@naver.com>
2022-07-04 16:01:16 +0900

    add restaurantId in createRestaraunt

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -56,6 +56,7 @@ export class RestaurantService {
       await this.restaurants.save(newRestaurant);
       return {
         ok: true,
+        restaurantId: newRestaurant.id,
       };
     } catch (error) {
       return {
